e54ad8ce7cd2a59a57405f78c21e3bfffa37193d
Maxw2 <635433387@qq.com>
2018-09-08 22:40:25 +0800

    更新README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 2048
+<ol>
+    <li><a href='http://htmlpreview.github.io/?https://github.com/maxw2/webGame/blob/master/2048/index.html'>2048
+    </li>
+</ol>
--- Tetris/index.js
+++ Tetris/index.js
@@ -1,16 +1,8 @@
 var Tetris = function(id){
     this.id = id
     this.bGround = []
-    this.block = {
-        i:[],
-        j:[],
-        l:[],
-        o:[],
-        s:[],
-        z:[],
-        t:[]
-    }
-
+    this.block = {}
+    this.timer
 
 
     this.init()
@@ -19,56 +11,19 @@ var Tetris = function(id){
 }
 Tetris.prototype = {
     init:function(){
-        this.getData()
         this.template()
-
-
-
-    },
-    //
-    getData:function(){
         this.getbGroundData()
-        this.getBlockData()    
-    },
-    getbGroundData:function(){
-        for(let i = 0;i < 20;i++){
-            this.bGround[i] = []
-            for(let j = 0;j < 10;j++){
-                this.bGround[i].push(0)
-            }
-        }
-    },
-    getBlockData:function(){
-        this.block.i = [[0,1,0],
-                        [0,1,0],
-                        [0,1,0],
-                        [0,1,0]]
-
-        this.block.j = [[0,1,0],
-                        [0,1,0],
-                        [1,1,0]]
-
-        this.block.l = [[0,1,0],
-                        [0,1,0],
-                        [0,1,1]]
-
-        this.block.o = [[1,1],
-                        [1,1]]
-
-        this.block.s = [[0,1,1],
-                        [1,1,0]]
         
-        this.block.z = [[1,1,0],
-                        [0,1,1]]
+        this.stareGame()
+        
 
-        this.block.t = [[0,1,0],
-                        [1,1,1]]
+
+
     },
     //
     template:function(){
         this.id.innerHTML = '<ul class="bGround"></ul>'
         var ul = this.id.querySelector('.bGround')
-
         for(let i = 0;i < 20;i++){
             var a = this._creatEle('li',ul,{'class':'row' + i})
             for(let j = 0;j < 10;j++){
@@ -76,15 +31,115 @@ Tetris.prototype = {
             }
         }
     },
+    getbGroundData:function(){
+        for(let i = 0;i < 20;i++){
+            this.bGround[i] = []
+            for(let j = 0;j < 10;j++){
+                this.bGround[i][j] = 0
+            }
+        }
+    },
     //
-    listener:function(){
+    //
+    stareGame:function(){
+        this.getBlockData()
+        this.refreshFrame()
+
+
+
+
 
     },
+    
+    getBlockData:function(){
+        this.block = new Array(4)
+        var num = (Math.floor(Math.random()*20)+1)%7;
+        console.log(num)
+        switch(num){
+            //i
+            case 0:
+                this.block[0] = {row:0,col:5}
+                this.block[1] = {row:1,col:5}
+                this.block[2] = {row:2,col:5}
+                this.block[3] = {row:3,col:5}
+                break;
+            //J
+            case 1:
+                this.block[0] = {row:0,col:5}
+                this.block[1] = {row:1,col:5}
+                this.block[2] = {row:2,col:5}
+                this.block[3] = {row:2,col:4}
+                break;
+            //L    
+            case 2:
+                this.block[0] = {row:0,col:5}
+                this.block[1] = {row:1,col:5}
+                this.block[2] = {row:2,col:5}
+                this.block[3] = {row:2,col:6}
+                break;
+            //O
+            case 3:
+                this.block[0] = {row:0,col:5}
+                this.block[1] = {row:0,col:6}
+                this.block[2] = {row:1,col:5}
+                this.block[3] = {row:1,col:6}
+                break;
+            //S
+            case 4:
+                this.block[0] = {row:0,col:6}
+                this.block[1] = {row:0,col:5}
+                this.block[2] = {row:1,col:5}
+                this.block[3] = {row:1,col:4}
+                break;
+            //Z
+            case 5:
+                this.block[0] = {row:0,col:4}
+                this.block[1] = {row:0,col:5}
+                this.block[2] = {row:1,col:5}
+                this.block[3] = {row:1,col:6}
+                break;
+            case 6:
+                this.block[0] = {row:0,col:5}
+                this.block[1] = {row:1,col:4}
+                this.block[2] = {row:1,col:5}
+                this.block[3] = {row:1,col:6}
+                break;
+        }
+    },
+    
+
+    //刷新帧数
+    //随机方框 随机方位
     //碰撞体积
     //向下坠落
     //
+    refreshFrame:function(){
+        var that = this
+        
+        this.timer = setInterval(function(){
+            
+            that.blockDown()
 
+            console.log(that.bGround)
+        },500)
+    },
+    //方块下坠
+    blockDown:function(){
+        for(let i = 0;i < this.block.length;i++){
+            var row = this.block[i].row
+            var col = this.block[i].col
+            this.bGround[row][col] = 1
+            this.bGround[row][col].className = '.block'   
+            
+        }
 
+        clear
+    },
+    
+    //方块旋转
+    blockRotate:function(){
+    //写自闭了 不想写了      
+    },
 
 
 
